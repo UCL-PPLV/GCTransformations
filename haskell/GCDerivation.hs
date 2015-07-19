c9aac704f24b5bcca5fa8ccd14edb88b53550e29
Ilya Sergey <ilya.sergey@gmail.com>
2015-07-19 18:18:21 +0200

    testing on examples from the paper

--- haskell/GCDerivation.hs
+++ haskell/GCDerivation.hs
@@ -269,3 +269,59 @@ expose_rcd :: (WavefrontDimension, ProtectionDimension, PolicyDimension) =>
               AL -> [LogEntry] -> [ObjectOrNull]
 expose_rcd als p = nub $ expose_rc als p ++ expose_d als p
 
+{-----------------------------------------------------------------}
+{--                    The  examples from the paper             --}
+{-----------------------------------------------------------------}
+
+{- Example 2.3 -}
+
+-- Initial objects
+r1, a, b, c, d, e :: Object
+r1 = O "r1" $ fromList $ 
+              [("f1", Nothing), ("f2", Just "A"), ("f3", Just "E")]
+a  = O "A" $ fromList $ 
+             [("f1", Nothing), ("f2", Nothing), ("f3", Nothing)]
+b = O "B" empty
+c = O "C" $ fromList [("f", Just "B")]
+d = O "D" $ fromList [("f", Just "E")]
+e = O "E" empty
+
+-- Final set of the objects
+al_final = [r1, a, b, c, d, e]
+
+prefix_pe :: [LogEntry]
+prefix_pe = [
+  LE T "r1" "f2" (Just "A") (Just "A"),
+  LE T "A"  "f1" Nothing    Nothing,
+  LE T "r1" "f3" Nothing    Nothing,
+  --
+  LE M "r1" "f1" Nothing    (Just "B"),
+  LE M "A"  "f1" Nothing    (Just "B"),
+  LE M "r1" "f3" Nothing    (Just "E"),
+  --
+  LE M "A"  "f2" (Just "C")  Nothing,
+  LE M "r1" "f1" (Just "B")  Nothing,
+  LE T "A"  "f2" Nothing     Nothing,
+  --
+  LE T "r1" "f1" Nothing    Nothing,
+  LE M "A"  "f3" (Just "D") Nothing,
+  LE M "A"  "f1" (Just "B") Nothing,
+  --
+  LE T "A"  "f3" Nothing    Nothing]
+
+-- Computing the wavefront
+
+wf_pe :: [(ObjId, FName)]
+wf_pe = wavefront prefix_pe 
+-- try wf_pre from the interpreter
+-- Okay, that works!
+
+{- Example 3.1 -}
+ex_apex_res :: [ObjId]
+ex_apex_res = expose_apex al_final prefix_pe 
+-- OK, that works too
+
+
+
+
+-- TODO: examples from the rest of the paper
