c8ec84b46432d8d3076dafa159e6d3da382d6c9b
Ilya Sergey <ilya.sergey@gmail.com>
2015-07-30 14:25:09 +0200

    renamings

---
+++ haskell/Example58.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE NullaryTypeClasses #-}
+
+module GCExample54 where
+
+import Data.Map as M
+import Data.Maybe as MB
+import Data.List as L
+import Control.Monad
+import Data.Maybe
+import GCDerivation
+import GCExample23
+
+{- Example 5.8 -}
+
+instance WavefrontDimension where
+  fl _ o = True
+
+instance PolicyDimension where
+  lr _ o = o /= "r1"
+
+instance ProtectionDimension where
+  is = const True
+
+instance ThresholdDimension where
+  dk = Ind 1
+
+pref3 = pre 3 prefix_pe
+
+mp_b = m_plus al_final "B" pref3
+-- 0
+
+mm_b = m_minus al_final "B" pref3
+-- 0
+
+ex_res56 = expose_ck al_final prefix_pe
+-- ["A","B","E"]
