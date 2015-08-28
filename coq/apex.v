13a9e8153c23b2dd8bef847ebb06d76d68901c24
Ilya Sergey <ilya.sergey@gmail.com>
2015-08-28 18:59:59 +0200

    wip

--- coq/apex.v
+++ coq/apex.v
@@ -13,7 +13,7 @@ Section ApexAlgo.
 Variables (h0 : heap) (g0: graph h0).
 
 (* A collector log p will all unique entires *)
-Variables  (p : log) (upf : uniq p).
+Variables  (p : log).
 
 (* Final heap and graph for the log p with the corresponding certificate epf *)
 Variables (h : heap) (g: graph h).
@@ -27,25 +27,113 @@ Notation "o '#' f" := (nth null (fields g o) f)
 (*    Apex procedure for exposing reachable objects in the graph  *)
 (******************************************************************)
 
-(* Eval compute in let lst := [:: 1; 2; 3] in take (index 1 lst) lst. *)
+(* An auxiliary function that generates all prefixes for elements of a
+list l0 *)
 
-(* 
+Fixpoint zip_num (l : log) n := 
+  match l with 
+  | [::]  => [::]
+  | e::l' => (e, n) :: zip_num l' (n.+1)
+  end.
 
-To simplify the implementation, we ensure that all entries in the log
-are unique (see the assumption 'upf'). Therefore, the prefix 'prepi'
-is unambiguously defined for each entry in the log, and is computed by
-an entry itself, not a specific index.
-*)
+
+Lemma zip_num_elems l: forall e n,
+  (e, n) \in zip_num l (size l - size l) ->
+  (size l <= size l) -> 
+  (if n == (size l - size l) then nth e l n == e else true) ->
+  nth e l n = e.
+Proof.
+elim: l {-1 3 4 7}l=>// x xs Hi l e n /=D H1 H2; rewrite !inE /=in D H1 H2.
+
+(* TODO: What is missing is the knowledge that x::xs is a suffix of l *)
+case/orP:D.
+- by case/eqP=>Z1 Z2; subst n; rewrite eqxx in H2; move/eqP: H2. 
+
+rewrite subnSK// =>G. apply: (Hi _ _ _ G); first by apply: ltnW.
+
+case: ifP=>// /eqP Z. subst n.
+
+
+
+Admitted.
+
+
+
+
+Fixpoint prefs_els_rec (l0 l : log) n := 
+  match l with 
+  | [::]  => [::]
+  | e::l' => (take n l0, e, n) :: prefs_els_rec l0 l' (n.+1)
+  end.
+
+Definition prefs_els l := prefs_els_rec l l 0.
+ 
+Lemma prefs_take1' l: forall pr e n,
+  (pr, e, n) \in (prefs_els_rec l l (size l - size l)) ->
+  (size l <= size l) -> 
+  (pr = take n l).
+Proof.
+rewrite/prefs_els.
+elim/list_ind: l {-2 4 5}l=>// x xs Hi l pr e n/= D H.
+rewrite !inE /=in D; case/orP: D; first by case/eqP=>Z1 Z2 Z3; subst pr e n. 
+by rewrite subnSK// =>G; apply: (Hi _ _ _  _ G); apply: ltnW.
+Qed.
+
+Lemma prefs_take1 l pr e n:
+   (pr, e, n) \in prefs_els l -> pr = take n l.
+Proof.
+move=>H. 
+have N: forall n, n - n = 0 by elim.
+have X: (pr, e, n) \in (prefs_els_rec l l (size l - size l)).
+- by rewrite N. 
+by apply: (prefs_take1' X); apply: leqnn.
+Qed.
+
+
+(* Lemma prefs_take2' l: forall pr e n, *)
+(*   (pr, e, n) \in (prefs_els_rec l l (size l - size l)) -> *)
+(*   (size l <= size l) -> *)
+(*   (exists s, l = s ++ l) -> *)
+(*   drop n l = e :: (drop n.+1) l. *)
+(* Proof. *)
+(* rewrite/prefs_els. *)
+(* elim: l {-2 4 5 8}l=>//x xs Hi l pr e n/=D H1 H2.  *)
+(* rewrite subnSK// in D. *)
+(* case/orP:D=>//G. *)
+(* case/eqP: G=>Z1 Z2 Z3; subst pr e n. *)
+(* admit. *)
+
+(* drop (size l - size l) l = x :: drop (size l - size l).+1 l *)
+
+(* apply: (Hi _ _ _ _ G);clear G Hi; first by apply: ltnW. *)
+
+
+(* apply:(Hi l pr e n)=>{Hi}=>//; do?[by apply: ltnW]. *)
+
+
+
+(* (* case/orP: D=>//=. *) *)
+(* (* case/eqP=>Z1 Z2 Z3; subst pr e n=>/=. *) *)
+
+
+(* (* (* apply: Hi; last by apply: ltnW. *) *) *)
+(* (* (* rewrite subnSK. *) *) *)
+(* (* (* rewrite !inE /=in D; case/orP: D; first by case/eqP=>Z1 Z2 Z3; subst pr e n.  *) *) *)
+(* (* (* by rewrite subnSK// =>G; apply: (Hi _ _ _  _ G); apply: ltnW. *) *) *)
+
+
+(* Qed. *)
 
 Definition expose_apex : seq ptr := 
-  [seq let o := (source pi) in
-       let f := (fld pi)    in 
-       o#f | pi <- p &
-             let k     := (kind pi)             in   
-             let o     := (source pi)           in
-             let f     := (fld pi)              in   
-             let prepi := (take (index pi p) p) in
-             (kindMA k) && ((o, f) \in wavefront prepi)].
+  [seq let pi := pei.1.2   in
+       let o  := source pi in
+       let f  := fld pi    in 
+       o#f | pei <- prefs_els p &
+             let: (pre, pi, i) := pei         in   
+             let k             := (kind pi)   in   
+             let o             := (source pi) in
+             let f             := (fld pi)    in   
+             (kindMA k) && ((o, f) \in wavefront pre)].
 
 (* Now, we have to show that only reachable objects are exposed by the
 'expose_apex' procedure... *)
