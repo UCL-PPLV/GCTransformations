b68faee0038cee78c2f4d538f9cf1526467d6ba4
Ilya Sergey <ilya.sergey@gmail.com>
2015-09-06 00:30:18 +0200

    stub added

---
+++ coq/partitions.v
@@ -0,0 +1,19 @@
+Require Import Ssreflect.ssreflect Ssreflect.ssrbool Ssreflect.ssrnat.
+Require Import Ssreflect.eqtype Ssreflect.ssrfun Ssreflect.seq.
+Require Import MathComp.path.
+Require Import Eqdep pred prelude idynamic ordtype pcm finmap unionmap heap coding. 
+Require Import hgraphs logs.
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive. 
+
+Section Partitions.
+
+(* TODO: Implement a record interface for object space partitions *)
+
+Structure Partition2 := Par2 {
+  
+
+}.
+
+End Partitions.
