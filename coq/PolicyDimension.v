d745c0691837402e5ca838f1ec9989b71a552954
Ilya Sergey <ilya.sergey@gmail.com>
2015-09-10 12:40:48 +0200

    expose_c sketched

--- coq/PolicyDimension.v
+++ coq/PolicyDimension.v
@@ -21,17 +21,22 @@ Variables  (p : log).
 Variables (h : heap) (g: graph h).
 Variable (epf : executeLog g0 p = Some (ExRes g)).
 
-Variables (wp pp : par2).
+Variables (wp polp prp : par2).
 
 (* Wavefront partition *)
 Notation FL := (pr1 wp).
 Notation OL := (pr2 wp).
 
 (* Policy partition *)
-Notation SR := (pr1 pp).
-Notation LR := (pr2 pp).
+Notation SR := (pr1 polp).
+Notation LR := (pr2 polp).
+
+(* Protection partition *)
+Notation IS := (pr1 prp).
+Notation DS := (pr2 prp).
 
 Notation w_gt := (W_gt g wp).
+Notation w_lt := (W_lt g wp).
 
 (* Consider expose_r from Section 5.2.1 of the paper, taking IS =
    True, so it's not mentioned so far. In this shape, expose_r is exactly
@@ -47,11 +52,84 @@ Definition expose_r : seq ptr :=
                let k             := (kind pi)   in   
                let o             := (source pi) in
                let f             := (fld pi)    in   
-               [&& (kindMA k), ((o, f) \in w_gt pre) &
-                   SR o]].
+               let n             := (new pi)    in   
+               [&& (kindMA k), ((o, f) \in w_gt pre),
+                   SR o, IS n & IS (o#f@g)]].
 
-(* TODO: use seq.undup to remove duplicates  *)
 
-Search _ (undup _) (size _).
+(* A lemma, similar to the one, proved for expose_apex *)
+
+Lemma expose_r_fires et l1 l2 : 
+  let o := source et in
+  let f := fld    et in
+  let x := o # f @ g in
+  p = l1 ++ et :: l2  ->
+  kind et == T        ->
+  (forall q, IS q) -> SR o -> 
+  x \in tracedTargets p ++ expose_r.
+Proof.
+move=>/=E K H1 H2.
+rewrite mem_cat; apply/orP.
+case: (traced_objects epf E K); [left | right].
+- by apply/tracedTargetsP; exists et, l1, l2.
+case/hasP: b=>ema D/andP[K2]/andP[/eqP E2]/andP[/eqP E3]/eqP E4; rewrite E2 E3.
+case: (prefix_wavefront e0 D E K)=>i[pre][G1]/(w_gt_approx epf wp) G2.
+apply/mapP'; exists (pre, ema, i)=>//=; split=>//.
+by apply/mem_filter'; rewrite !H1 -!(andbC true) K2/= -E2 -E3 G2; split.
+Qed.
+
+Definition M_plus o : nat := size (undup 
+     [seq let pre := proj1_sig pe.1.1 in pre
+                  | pe <- prefixes e0 p &
+                   let: (pre, pi, _) := pe in   
+                   [&& (kindMA (kind pi)), (new pi) == o, 
+                   ((source pi, fld pi) \in w_gt pre) & LR (source pi)]]).
+
+
+Definition M_minus o : nat := size (undup 
+     [seq let pre := proj1_sig pe.1.1 in pre
+                 | pe <- prefixes e0 p &
+                   let: (pre, pi, _) := pe in   
+                   [&& (kindMA (kind pi)), (new pi) == o, 
+                   ((source pi, fld pi) \in w_lt pre) & LR (source pi)]]).
+
+Definition expose_c : seq ptr := 
+     [seq new pi | pi <- p &
+                   let n := new pi in
+                   [&& (M_plus n > M_minus n) & IS n]].
+
+
+Lemma expose_c_fires et l1 l2 : 
+  let o := source et in
+  let f := fld    et in
+  let x := o # f @ g in
+  p = l1 ++ et :: l2  ->
+  kind et == T        ->
+  (forall q, IS q) -> LR o -> 
+  x \in tracedTargets p ++ expose_c.
+Proof.
+move=>/=E K H1 H2.
+rewrite mem_cat; apply/orP.
+case: (traced_objects epf E K); [left | right].
+- by apply/tracedTargetsP; exists et, l1, l2.
+case/hasP: b=>ema D/andP[K2]/andP[/eqP E2]/andP[/eqP E3]/eqP E4; rewrite E2 E3. 
+rewrite E2 ?E3 in H2 E4.
+
+(* Okay, now we need to figure out, why the inequlaity (M_plus n >
+   M_minus n) exposes the sound superset of missed objects wrt. the
+   prefix p. Intuitively, this is by the following argument.
+
+   M_plus is number of prefixes of p, where 'o' occurs as a target,
+   and some of its corresponding source fields values are
+   traced. M_plus is a number of prefixes of p, where 'o' occurs as a
+   target, and *all* of its source fields values are traced. The
+   difference between the two being equal zero indicates that all
+   affected object fields of interest were in fact observed by tracing
+   (M_minus makes sure of that thanks to the all-check in the
+   definition of W_lt).
+
+ *)
+
+Admitted.
 
 End PolicyDimension.
